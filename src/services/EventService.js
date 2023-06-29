63c284a748b423bf92d92b8a4b227647a3519f7f
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2023-06-29 12:51:14 +0500

    api calls with axios

---
+++ src/services/EventService.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL:
+    'https://my-json-server.typicode.com/Jakhongirkhoja-Bakhodirov/real-world-vue-3',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
+
+export default {
+  getEvents() {
+    return apiClient.get(
+      'https://my-json-server.typicode.com/Jakhongirkhoja-Bakhodirov/real-world-vue-3/events/'
+    );
+  },
+};
