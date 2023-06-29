80028c7421e4f2efde15b0ef632de6a052fe70dc
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2023-06-29 16:13:16 +0500

    dynamic routing on event details

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="event-cart"></div>
-  <span>@ {{ event.time }} on {{ event.date }}</span>
-  <h4>{{ event.title }}</h4>
+  <router-link :to="{ name: 'EventDetails', params: { id: event.id } }">
+    <div class="event-cart"></div>
+    <span>@ {{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </router-link>
 </template>
 
 <script>
@@ -21,19 +23,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* .event-card {
-  padding: 20px;
-  width: 250px;
-  cursor: pointer;
-  border: 1px solid #39495c;
-  margin-bottom: 18px;
-}
-
-.event-card:hover {
-  transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-} */
-
 .event-card {
   padding: 20px;
   width: 250px;
