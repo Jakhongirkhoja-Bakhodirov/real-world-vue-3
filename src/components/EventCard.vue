efe4c2630b1b15232502caab2be6ad0e375f98e6
Jakhongirkhoja-Bakhodirov <masternuxtjstest@gmail.com>
2023-06-30 18:45:44 +0500

    fix on event card style

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -3,9 +3,10 @@
     class="event-link"
     :to="{ name: 'EventDetails', params: { id: event.id } }"
   >
-    <div class="event-cart"></div>
-    <span>@ {{ event.time }} on {{ event.date }}</span>
-    <h4>{{ event.title }}</h4>
+    <div class="event-card">
+      <span>@ {{ event.time }} on {{ event.date }}</span>
+      <h4>{{ event.title }}</h4>
+    </div>
   </router-link>
 </template>
 
