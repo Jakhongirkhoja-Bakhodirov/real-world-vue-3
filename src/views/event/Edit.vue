855ceaee191f6f5a2fc17e02307d6016bba58118
Jakhongirkhoja-Bakhodirov <masternuxtjstest@gmail.com>
2023-07-02 04:34:59 +0500

    create edit and register view

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -1,10 +1,24 @@
 <template>
   <div v-if="event">
     <h1>{{ event.title }}</h1>
-    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
-    <p>{{ event.description }}</p>
+    <div id="nav">
+      <router-link :to="{ name: 'EventDetails', params: { id } }"
+        >Details</router-link
+      >
+      |
+      <router-link :to="{ name: 'EventEdit', params: { id } }"
+        >Edit</router-link
+      >
+      |
+      <router-link :to="{ name: 'EventRegister', params: { id } }"
+        >Register</router-link
+      >
+    </div>
+    <p>Edit events</p>
   </div>
 </template>
+
+<style></style>
 
 <script>
 import EventService from '@/services/EventService';
