f6ce6fd30a457493885107c94773247495cd0feb
Jakhongirkhoja-Bakhodirov <masternuxtjstest@gmail.com>
2023-07-02 06:40:34 +0500

    nested-routes

---
+++ src/views/event/Layout.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <div id="nav">
+      <router-link :to="{ name: 'EventDetails', params: { id } }"
+        >Details</router-link
+      >
+      |
+      <router-link :to="{ name: 'EventEdit', params: { id } }"
+        >Edit</router-link
+      >
+      |
+      <router-link :to="{ name: 'EventRegister', params: { id } }"
+        >Register</router-link
+      >
+    </div>
+    <router-view :event="event"></router-view>
+  </div>
+</template>
+
+<script>
+import EventService from '@/services/EventService';
+export default {
+  name: 'EventLayout',
+  props: ['id'],
+  data() {
+    return {
+      event: null,
+    };
+  },
+  created() {
+    EventService.getEventById(this.id)
+      .then((response) => {
+        this.event = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
