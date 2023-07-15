64b69c7f3ea4d49ce135cbc5f2772c72f1cbb3d0
Jakhongirkhoja-Bakhodirov <masternuxtjstest@gmail.com>
2023-07-15 21:28:29 +0500

    scroll behavior

--- src/router/index.js
+++ src/router/index.js
@@ -99,6 +99,12 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  scrollBehavior(savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { top: 0 };
+  },
 });
 
 router.beforeEach((to, from) => {
